95a15fcf98cb56aed6fdabc7a954149b35424d98
hypnot1z <hypnot1z@proton.me>
2022-10-05 16:00:50 +0300

    minor update

--- src/UI/Button.js
+++ src/UI/Button.js
@@ -1,9 +1,13 @@
 import styles from './Button.module.css'
 
 function Button(props) {
-  const { children } = props
+  const { children, disabled = false } = props
   return (
-    <button {...props} className={styles.btn}>
+    <button
+      {...props}
+      className={`${styles.btn} ${disabled ? styles.noActive : ''}`}
+      disabled={disabled}
+    >
       {children}
     </button>
   )
